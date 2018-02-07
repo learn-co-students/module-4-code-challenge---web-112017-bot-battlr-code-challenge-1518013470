070451262ace728285aab9ba3a50bde3a410202a
siobhanpmahoney <siobhan.p.mahoney@gmail.com>
2018-02-07 11:01:12 -0500

    would like to discuss

--- src/containers/BotCollection.js
+++ src/containers/BotCollection.js
@@ -1,15 +1,23 @@
 import React from "react";
-import BotCard from "../components/BotCard";
+// import BotCard from "../components/BotCard";
+import AltBotCard from "../components/AltBotCard"
 
 class BotCollection extends React.Component {
-  //your code here
+  constructor(props) {
+    super(props)
+  }
 
   render(){
+
   	return (
   	  <div className="ui four column grid">
     		<div className="row">
     		  {/*...and here..*/}
     		  Collection of all bots
+          {this.props.allBots.map((bot) => {
+            return <AltBotCard bot={bot} key={bot.id} handleBotEnlist={this.props.handleBotEnlist}/>
+          })}
+
     		</div>
   	  </div>
   	);
--- src/containers/BotsPage.js
+++ src/containers/BotsPage.js
@@ -1,12 +1,55 @@
 import React from "react";
+import BotCollection from './BotCollection'
+import YourBotArmy from './YourBotArmy'
 
 class BotsPage extends React.Component {
   //start here with your code for step one
 
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      allBots: [],
+      yourBotArmy: []
+    }
+  }
+
+  componentDidMount() {
+    fetch("https://bot-battler-api.herokuapp.com/api/v1/bots")
+    .then(response => response.json())
+    .then(json => this.setState({
+      allBots: json
+    }))
+  }
+
+  handleBotEnlist = (bot) => {
+    console.log('hit')
+    let currentArmy = this.state.yourBotArmy
+    if (!this.state.yourBotArmy.includes(bot)) {
+      this.setState({
+        yourBotArmy: [...currentArmy, bot]
+      })
+    }
+    else {
+      alert("Already enlisted")
+    }
+  }
+
+  handleBotRemove = (bot) => {
+    let currentArmy = this.state.yourBotArmy
+    if (this.state.yourBotArmy.includes(bot)) {
+      currentArmy = this.state.yourBotArmy.splice(this.state.yourBotArmy.indexOf(bot), 1)
+      this.setState({
+        yourBotArmy: [...currentArmy]
+      })
+    }
+  }
+
   render() {
     return (
       <div>
-        {/* put your components here */}
+        <BotCollection allBots = {this.state.allBots} handleBotEnlist = {this.handleBotEnlist} />
+        <YourBotArmy yourBotArmy={this.state.yourBotArmy} handleBotRemove={this.handleBotRemove}/>
       </div>
     );
   }
