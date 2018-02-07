b25eb2eeaa95ade943eecf951176243b7cacc8fc
ddbrennan <ddbren@gmail.com>
2018-02-07 10:57:28 -0500

    made prettier

--- src/components/BotSearch.js
+++ src/components/BotSearch.js
@@ -2,9 +2,9 @@ import React from 'react'
 
 const BotSearch = (props) => {
   return (
-    <div>
-      <h2>Filter The Bots</h2>
-      <select onChange={ props.changeFilter } value={ props.filter }>
+    <div className="filter-container">
+      <h2>Filter The Bots:</h2>
+      <select className="filter-bar" onChange={ props.changeFilter } value={ props.filter }>
         <option value="">None</option>
         <option>Assault</option>
         <option>Defender</option>
